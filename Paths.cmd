d489524a8d8ff509578e22c063c53a41aca563d6
ApacheThunder <spenserwhaley@gmail.com>
2024-07-10 00:33:06 -0500

    Removed test file.

--- Paths.cmd
+++
@@ -1,6 +0,0 @@
-rem set PATH=C:\devkitPro_gba\msys\bin;C:\devkitPro_gba\devkitARM\bin;%PATH%
-set PATH=C:\devkitPro_gba\msys\bin;C:\devkitPro_gba\devkitARM_r47\bin;%PATH%
-set DEVKITARM=/c/devkitPro_gba/devkitARM_r47
-set DEVKITPRO=/c/devkitPro_gba
-set LIBGBA=/c/devkitPro_gba/libgba
-cls
